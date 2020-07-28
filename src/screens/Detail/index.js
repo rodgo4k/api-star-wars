09582e2bed78beba3fe4a328dd62bad7513b2a33
Rodrigo Prestes <rodrigoaprestes@gmail.com>
2020-07-28 15:40:18 -0300

    api

--- src/screens/Detail/index.js
+++ src/screens/Detail/index.js
@@ -19,30 +19,30 @@ const Detail = () => {
 
     if(person.gender === "male") {
         return  <Container>
-                <h1>{person.name}</h1>
-                <h3>
-                    {person.name} is {person.height}cm tall and his mass is {person.mass}kg. His hair is {person.hair_color} and 
-                    his eyes are {person.eye_color}. He was born in {person.birth_year}.
-                </h3>
+                    <h1>{person.name}</h1>
+                    <h3>
+                        {person.name} is {person.height}cm tall and his mass is {person.mass}kg. His hair is {person.hair_color} and 
+                        his eyes are {person.eye_color}. He was born in {person.birth_year}.
+                    </h3>
                 </Container>
     }
 
     if(person.gender === "female") {
         return  <Container>
-                <h1>{person.name}</h1>
-                <h3>
-                    {person.name} is {person.height}cm tall and her mass is {person.mass}kg. Her hair is {person.hair_color} and 
-                    her eyes are {person.eye_color}. She was born in {person.birth_year}.
-                </h3>
+                    <h1>{person.name}</h1>
+                    <h3>
+                        {person.name} is {person.height}cm tall and her mass is {person.mass}kg. Her hair is {person.hair_color} and 
+                        her eyes are {person.eye_color}. She was born in {person.birth_year}.
+                    </h3>
                 </Container>
     }
     if(person.gender === "n/a") {
         return  <Container>
-                <h1>{person.name}</h1>
-                <h3>
-                    {person.name} is {person.height}cm tall and their mass is {person.mass}kg. Their hair is {person.hair_color} and 
-                    their eyes are {person.eye_color}. They were born in {person.birth_year}.
-                </h3>
+                    <h1>{person.name}</h1>
+                    <h3>
+                        {person.name} is {person.height}cm tall and their mass is {person.mass}kg. Their hair is {person.hair_color} and 
+                        their eyes are {person.eye_color}. They were born in {person.birth_year}.
+                    </h3>
                 </Container>
     }
 };
